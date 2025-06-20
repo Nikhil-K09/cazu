bbc2d4cd7d8cf0309057c8a1cf338eab43c7659e
Nikhil Kalavagunta <kalavaguntanikhil@gmail.com>
2025-06-20 14:47:49 +0530

    Update admin_cred.py

--- admin_cred.py
+++ admin_cred.py
@@ -1,8 +1,1 @@
-from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://admin:nikhil@cluster0.yjnzzzx.mongodb.net/")
-db = client['cazu_services']
-admins_col = db['admins']
-
-admins_col.insert_one({'username': 'admin', 'password': 'admin123'})
-print("Admin account created.")
